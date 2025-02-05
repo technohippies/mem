806363ec3084cac4d9bea81d14a9fe4b653d9c57
technohippies <hi@x.technohippies>
2025-02-05 18:53:05 +0200

    chore: remove outdated Header component and duplicate AppRoutes

--- src/AppContent.tsx
+++ src/AppContent.tsx
@@ -4,13 +4,11 @@ import { AuthWrapper } from './components/auth/AuthWrapper';
 import { HomePage } from '@/components/pages/HomePage';
 import { DeckPage } from '@/components/pages/DeckPage';
 import { StudyPage } from '@/components/pages/StudyPage';
-import { Header } from '@/components/core/Header';
 import { Toaster } from 'sonner';
 
 const AppContent: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col bg-neutral-900 text-neutral-100">
-      <Header />
       <main className="flex-1">
         <Routes>
           <Route path="/" element={<HomePage />} />
