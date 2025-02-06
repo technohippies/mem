9c85b8500632ac42af775ee0743a2d5255e3c24e
technohippies <hi@x.technohippies>
2025-02-06 17:32:48 +0200

    feat: customize wallet options in AppKit modal

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,28 @@ createAppKit({
   projectId: 'anki-farcaster',
   networks: [mainnet, optimism],
   metadata: {
-    name: 'Anki Farcaster',
+    name: 'Mem',
     description: 'Spaced repetition learning with Farcaster',
     url: window.location.origin,
     icons: []
   },
+  // Featured wallets (MetaMask and Trust Wallet)
+  featuredWalletIds: [
+    'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', // MetaMask
+    '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0', // Trust Wallet
+    'joyid', // JoyID
+  ],
+  // Enable WalletConnect QR code
+  enableWalletConnect: true,
+  // Show all wallets button only on mobile
+  allWallets: 'ONLY_MOBILE',
+  // Customize connection methods order
   features: {
-    analytics: true
+    analytics: true,
+    connectMethodsOrder: ['wallet', 'social'],
+    // Disable onramp and swaps since we don't need them
+    onramp: false,
+    swaps: false
   }
 });
 
